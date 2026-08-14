ec658fec07a91d703bdc0022f5e560c73b2a9641
zshuai8 <zshuai8@vt.edu>
2026-08-14 08:21:07 +0000

    Deploying to gh-pages from @ zshuai8/zshuai8.github.io@4253a7e260226a7d58092b7152ef1b70deca9cee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about-me",
-    title: "About Me",
+    id: "nav-shuaicheng-zhang",
+    title: "Shuaicheng Zhang",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
